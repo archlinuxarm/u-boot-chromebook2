394c5df3386ac42dc88c53cf011be20a23fc80b2
Gabe Black <gabeblack@chromium.org>
2013-03-15 23:38:48 -0700

    cros: temp: Allow use of stdint.h
    This is a test patch to deal with Gabe's change to using stdint.h.
    
    Change-Id: Ibc19b586a233f2887c5bb08a7488869d63857fba

--- cros/include/biosincludes.h
+++ cros/include/biosincludes.h
@@ -11,15 +11,4 @@
 #ifndef BIOSINCLUDES_H
 #define BIOSINCLUDES_H
 
-#define UINT64_C(v) ((uint64_t) v)
-
-#include <compiler.h>
-#include <inttypes.h>
-
-#define INLINE inline
-#define POSSIBLY_UNUSED
-
-#define UINT64_RSHIFT(v, shiftby) (((uint64_t)(v)) >> (shiftby))
-#define UINT64_MULT32(v, multby)  (((uint64_t)(v)) * ((uint32_t)(multby)))
-
 #endif /* BIOSINCLUDES_H */
