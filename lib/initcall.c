5530087b04be9976f776e37c8b3c33e0e39d3c13
Simon Glass <sjg@chromium.org>
2013-06-02 14:19:48 -0700

    gen: Display pre-relocation addresses in initcall
    The addresses currently displayed are not very useful without knowing the
    relocation offset. Ideally we would have a list of function names, but
    for the moment subtracting the relocation offset lets us look up the
    address in System.map.
    
    BUG=none
    TEST=manual
    Adjust one of the post-relocation initcalls to return 1 (indicating
    failing), boot on link and see that we now get a useful address.
    
    Change-Id: I54df5fadf336dba8a2b35e08533b09a6ae682461
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/57636
    Reviewed-by: Che-Liang Chiou <clchiou@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>

--- lib/initcall.c
+++ lib/initcall.c
@@ -30,12 +30,13 @@ int initcall_run_list(init_fnc_t init_sequence[])
 	init_fnc_t *init_fnc_ptr;
 
 	for (init_fnc_ptr = init_sequence; *init_fnc_ptr; ++init_fnc_ptr) {
-		debug("initcall: %p\n", *init_fnc_ptr);
-		if ((*init_fnc_ptr)()) {
-			unsigned long reloc_ofs = 0;
+		unsigned long reloc_ofs = 0;
 
-			if (gd->flags & GD_FLG_RELOC)
-				reloc_ofs = gd->reloc_off;
+		if (gd->flags & GD_FLG_RELOC)
+			reloc_ofs = gd->reloc_off;
+		debug("initcall: %p\n", (char *)*init_fnc_ptr - reloc_ofs);
+		if ((*init_fnc_ptr)()) {
+
 			printf("initcall sequence %p failed at call %p\n",
 			       init_sequence, (char *)*init_fnc_ptr - reloc_ofs);
 			return -1;
