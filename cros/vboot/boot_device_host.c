b3e3e2101573563d9c32d0e4f102642e1d21b0be
Simon Glass <sjg@chromium.org>
2013-07-02 15:21:20 +0900

    cros: sandbox: Add boot_device for sandbox 'host' device
    This allows us to boot a kernel from the host filesystem.
    
    BUG=chromium:244019
    BRANCH=none
    TEST=manual
    With other changes, see that vboot can load a kernel from a Chrome OS
    image and boot it.
    
    Change-Id: I391368adb88f9594213701ff162b08af5c75dd59
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/61844

---
+++ cros/vboot/boot_device_host.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+#include <common.h>
+#include <sandboxblockdev.h>
+#include <cros/common.h>
+#include <cros/boot_device.h>
+
+static int boot_device_host_start(uint32_t disk_flags)
+{
+	/* We expect to have at least one MMC device */
+	return 1;
+}
+
+static int boot_device_host_scan(block_dev_desc_t **desc, int max_devs,
+			 uint32_t disk_flags)
+{
+	int index, found;
+
+	for (index = found = 0; index < max_devs; index++) {
+		block_dev_desc_t *scsi;
+
+		scsi = host_get_dev(index);
+		if (!scsi)
+			continue;
+
+		desc[found++] = scsi;
+	}
+	return found;
+}
+
+CROS_BOOT_DEVICE(host_interface) = {
+	.name = "host",
+	.type = IF_TYPE_HOST,
+	.start = boot_device_host_start,
+	.scan = boot_device_host_scan,
+};
